0e7db668bfadda0bc98c4bca1a0e82160660545f
Vonixxx <vonixxxwork@tuta.io>
2024-05-07 14:38:41 +0200

    formatting: master -> main

--- Script/Variables.hs
+++ Script/Variables.hs
@@ -3,4 +3,4 @@ module Variables where
 hide          = "&> /dev/null"
 linkVonixOS   = "github:Vonixxx/Vontoo"
 linkDisko     = "github:nix-community/disko"
-linkDiskSetup = "https://raw.githubusercontent.com/Vonixxx/Vontoo/master/system/configuration/disk/default.nix"
+linkDiskSetup = "https://raw.githubusercontent.com/Vonixxx/Vontoo/main/system/configuration/disk/default.nix"
