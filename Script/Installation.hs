0d72712761e115f5656099f5edff990ce837608b
Vonixxx <vonixxxwork@tuta.io>
2024-06-05 07:44:51 +0200

    test: disk config

--- Script/Installation.hs
+++ Script/Installation.hs
@@ -7,13 +7,13 @@ vontoo = "github:Vonixxx/Vontoo"
 disko  = "github:nix-community/disko#disko-install"
 
 installation = do
--- callCommand "\nlsblk --nodeps --output NAME,SIZE"
---
--- putStrLn "Pick the largest in size."
--- putStrLn "Either 'sda' or 'nvme0n1'"
--- putStr   "Disk: "
---
--- disk <- getLine
+ callCommand "\nlsblk --nodeps --output NAME,SIZE"
+
+ putStrLn "Pick the largest in size."
+ putStrLn "Either 'sda' or 'nvme0n1'"
+ putStr   "Disk: "
+
+ disk <- getLine
 ---
  callCommand "\nnix flake show github:Vonixxx/Vontoo"
 
@@ -24,10 +24,10 @@ installation = do
 ---
  putStrLn "\nSystem Installation...\n"
 
- callCommand $ printf "nix run '%s' -- --flake '%s#%s'"
+ callCommand $ printf "nix run '%s' -- --dry-run --flake '%s#%s' --disk main /dev/%s --write-efi-boot-entries"
                       disko
                       vontoo
                       user
- --                     disk
+                      disk
 
  putStrLn "\nSystem Installation - Successful."
