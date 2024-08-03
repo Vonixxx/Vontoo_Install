5cda2f71c47568281f0b417fabb1d20b11ee372a
Vonixxx <vonixxxwork@tuta.io>
2024-08-03 15:54:54 +0200

    rerouting kernel compilation object build files in order to avoid oom

--- System/SteamDeck/default.nix
+++ System/SteamDeck/default.nix
@@ -3,6 +3,12 @@
 {
  isoImage.isoBaseName = "Vontoo_SteamDeck";
 
+ boot.kernelPatches = [
+   {
+    extraMakeFlags = ["O=/home/Bubinka"];
+   }
+ ];
+
  jovian = {
    hardware.has.amd.gpu = true;
    steam.enable         = false;
